5ba96128be1b73f60107a683dcaf8a5cdea908e9
Jarno Luukkonen <luukkonen.jarno@gmail.com>
2017-12-09 19:03:33 +0200

    author and category models

---
+++ src/main/java/com/jarnoluu/domain/Author.java
@@ -0,0 +1,15 @@
+package com.jarnoluu.domain;
+
+import javax.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.AbstractPersistable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class Author extends AbstractPersistable<Long> {
+    String name;
+}
---
+++ src/main/java/com/jarnoluu/repository/AuthorRepository.java
@@ -0,0 +1,10 @@
+package com.jarnoluu.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.jarnoluu.domain.Author;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+    
+}
