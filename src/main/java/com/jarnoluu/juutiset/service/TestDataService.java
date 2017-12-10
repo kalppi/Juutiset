a58ea8065482bacbbb25b7f10114eeda5cb1822a
Jarno Luukkonen <luukkonen.jarno@gmail.com>
2017-12-10 23:31:15 +0200

    cleanup

--- src/main/java/com/jarnoluu/juutiset/service/TestDataService.java
+++ src/main/java/com/jarnoluu/juutiset/service/TestDataService.java
@@ -18,7 +18,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
-import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
