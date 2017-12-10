bc31674967c05b9b8a84a9bdb86f511f5be0d26e
Jarno Luukkonen <luukkonen.jarno@gmail.com>
2017-12-10 02:28:38 +0200

    fix?

--- src/main/java/com/jarnoluu/domain/Author.java
+++ src/main/java/com/jarnoluu/domain/Author.java
@@ -17,6 +17,6 @@ import org.springframework.data.jpa.domain.AbstractPersistable;
 public class Author extends AbstractPersistable<Long> {
     String name;
     
-    @ManyToMany(mappedBy="authors", fetch=FetchType.EAGER)
+    @ManyToMany(mappedBy="authors", fetch=FetchType.LAZY)
     private List<Article> articles;
 }
--- src/main/java/com/jarnoluu/repository/CategoryRepository.java
+++ src/main/java/com/jarnoluu/repository/CategoryRepository.java
@@ -3,8 +3,11 @@ package com.jarnoluu.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.jarnoluu.domain.Category;
+import java.util.Optional;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-    
+    @Transactional
+    public Optional<Category> findById(long id);
 }
